86e30b7f12d0c4a179413545de018b41d5ffcf8e
gongxianglong <76651639@qq.com>
2019-01-03 15:21:21 +0800

    bug

--- pages/repairs/verify/verify.js
+++ pages/repairs/verify/verify.js
@@ -1,11 +1,47 @@
 // miniprogram/pages/repairs/verify/verify.js
+
+const app = getApp();
+var user = require("../../../utils/user.js");
+var http = require("../../../request/httpRequest.js");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo: '',//用户登录信息
+    //导航栏
+    navbarTitle: [{
+      "columnName": '待确定',
+      "columnId": "1"
+    }, {
+      "columnName": '全部',
+      "columnId": "0"
+    }],
+    navbarActiveIndex: 0,
 
+    navPosition: [], //导航栏各个TAB的位置
+    scrollLeft: 0, //navbar scroll-view 使用
+
+    px2rpx: 2,
+    windowWidth: 375,//屏幕可用宽度
+    windowHeight: '',//屏幕可用高度
+    isHideLoadMore: true,//是否隐藏加载提示
+    isComplete: false,//是否全部加载完毕
+
+    endTipHidden: false,
+    endTip: '正在加载',
+
+    list: {},//把所有子项的数据都放在list里面
+    curListId: 0, //滑动到底部时，我们根据curListId知道当前需要请求那个对象
+
+    //请求参数 query
+    query: {
+      keys: '',
+      pageIndex: 1,
+      pageSize: 10,
+    },
   },
 
   /**
