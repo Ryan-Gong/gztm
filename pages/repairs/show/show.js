e8bbd9a1fc8ab9c8f19b1989af0699a86a557a6a
gongxianglong <76651639@qq.com>
2019-01-04 12:00:33 +0800

    修补bug

--- pages/repairs/show/show.js
+++ pages/repairs/show/show.js
@@ -1,5 +1,9 @@
 // miniprogram/pages/repairs/show/show.js
 const app = getApp();
+var utils = require("../../../utils/utils.js");
+var user = require("../../../utils/user.js");
+var http = require("../../../request/httpRequest.js");
+
 Page({
 
   /**
@@ -8,119 +12,37 @@ Page({
   data: {
     id: '',
     popErrorMsg: '',
-    site: {}, //站点信息
-    repairs: {},//维护信息
-    cate: ["安装", "调试", "故障", "维修", "故障维修", "巡检"],//数组
-    cateIndex: 0,
-    cateValue: '',
-    buildDate: '',//建站日期
-    editRight: false,//是否有编辑权限
+    loading: true,
+    repairs: '',//维护信息
+    
   },
-  req: function (id) {
-    var that = this;
-    var url = app.globalData.ApiUrl;
-    wx.request({
-      url: url + '/repairs',
-      data: {
-        "OTAId": "OTA",
-        "Parameter": { "id": id },
-        "Signature": ""
-      },
+  
+  /**
+   * 获取维护记录
+   */
+  getRepairs: function () {
+    let that = this;
+    //组装请求参数
+    let query = {
+      OTAId: that.data.userInfo.uname,
+      Parameter: {},
+      Signature: ''
+    };
+    //从API接口中查询数据
+    http.request({
+      url: app.globalData.ApiUrl + '/repairs/' + that.data.id,
       method: 'GET',
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-        var result = res.data.result
-        console.log(res);
-        //设置页面的数据
-        that.setData({
-          repairs: result,//维护信息
-          cateValue: result.fl,
-          buildDate: result.whsj
-        });
-      }
-    });
-  },
-  //选择建站日期
-  bindDateChange: function (e) {
-    this.setData({
-      buildDate: e.detail.value
-    })
-  },
-  //选择站点的状态
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
-    this.setData({
-      cateIndex: e.detail.value,
-      cateValue: this.data.cate[e.detail.value]
-    })
-  },
-  //检查表单是否完整
-  checkInput: function (value, msg) {
-    var that = this;
-    //3秒隐藏错误提示
-    var hide = function () {
-      var fadeOutTimeout = setTimeout(() => {
-        that.setData({ popErrorMsg: '' });
-        clearTimeout(fadeOutTimeout);
-      }, 3000);
-    };
-    //检测开始
-    var value = value.replace(/\s+/g, ''); //利用正则去除空格
-    if (value.length == 0) {
+      data: query,
+    }).then((res) => {
+      //console.log(res.result);
       that.setData({
-        popErrorMsg: msg
+        repairs: res.result,
       });
-      hide();
-      return false
-    };
-    return true;
-  },
-  //提交保存数据
-  formSubmit: function (e) {
-    var that = this;
-    var url = getApp().globalData.ApiUrl;
-    //检查权限
-    if (!editRight) {
-      wx.showToast({
-        title: '您无权限',
-        icon: 'loading',
-        duration: 3000
-      });
-      return false;
-    }
-    //let id = this.data.category[this.data.categoryIndex].id
-    var data = e.detail.value;
-    data.zt = that.data.siteStateValue; //站点状况
-    //检查表单是否完整
-    //1.检查名称
-    if (that.checkInput(data.zdm, '请输入磨面站点名称') == false) return false;
-    //2.检查地址
-    if (that.checkInput(data.zddz, '请输入磨面站点地址') == false) return false;
-    //3.检查经纬度
-    if (that.checkInput(data.jd, '请输入经度') == false) return false;
-    if (that.checkInput(data.wd, '请输入维度') == false) return false;
-
-    console.log(data);
-    //验证通过后，保存数据
-    return;
-    wx.request({
-      url: url + '/site/1',
-      data: {
-        "OTAId": "OTA",
-        "Parameter": e.detail.value,
-        "Signature": ""
-      },
-      method: 'POST',
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-        console.log(res);
-      }
+    }).catch((err) => {
+      console.log(err);
     });
   },
+  
   /**
    * 生命周期函数--监听页面加载
    */
